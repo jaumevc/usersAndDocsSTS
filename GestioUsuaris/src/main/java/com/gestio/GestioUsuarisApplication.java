db88713cce4487bfc1a5adfaa5d9b78f49b8567f
jaumevc@me.com <jaumevc@me.com>
2023-09-14 16:00:42 +0200

    1er commit

---
+++ GestioUsuaris/src/main/java/com/gestio/GestioUsuarisApplication.java
@@ -0,0 +1,21 @@
+package com.gestio;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@SpringBootApplication
+public class GestioUsuarisApplication implements CommandLineRunner{
+
+	private static Logger LOG = LoggerFactory.getLogger(GestioUsuarisApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(GestioUsuarisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.warn("Hola Mon Jaume");
+	}
+
+}
---
+++ GestioUsuaris/src/test/java/com/gestio/GestioUsuarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioUsuarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
