bd66e243a4bbd045dfcbed92073dfc111033757a
jaumevc@me.com <jaumevc@me.com>
2023-09-14 16:03:50 +0200

    commit 2

--- GestioUsuaris/src/main/java/com/gestio/GestioUsuarisApplication.java
+++ GestioUsuaris/src/main/java/com/gestio/GestioUsuarisApplication.java
@@ -16,6 +16,7 @@ public class GestioUsuarisApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		LOG.warn("Hola Mon Jaume");
+		LOG.info("Proves");
 	}
 
 }
